25768ced3ec35a8523d816f9c0175c4663829a1e
ben <ben@hal.arch.tamu.edu>
2014-02-27 14:10:20 -0600

    init

---
+++ Berksfile
@@ -0,0 +1,6 @@
+site :opscode
+
+cookbook 'rbenv', git: 'git://github.com/RiotGames/rbenv-cookbook.git'
+cookbook 'git', git: 'git://github.com/opscode-cookbooks/git.git'
+
+metadata
---
+++ recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: chef-dhcp-dash
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+# 
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'git'
+
+include_recipe 'rbenv'
+include_recipe 'rbenv::ruby_build'
+include_recipe 'rbenv::rbenv_vars'
+
+rbenv_ruby '1.9.3'
+
+rbenv_gem 'bundler' do
+  ruby_version '1.9.3'
+end
