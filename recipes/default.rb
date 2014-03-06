72a8befa33e3565e8fde908040fb9e3b519d645e
Victoria Blessing <vblessing@arch.tamu.edu>
2014-03-06 14:33:45 -0600

    made ruby install global

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,7 +12,9 @@ include_recipe 'rbenv'
 include_recipe 'rbenv::ruby_build'
 include_recipe 'rbenv::rbenv_vars'
 
-rbenv_ruby '2.1.0'
+rbenv_ruby '2.1.0' do
+  global true
+end
 
 rbenv_gem 'bundler' do
   ruby_version '2.1.0'
